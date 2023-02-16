9040a38f69fe92dda3fb696a96afcae8340b1f56
Firoms <edisonmessi@naver.com>
2023-02-16 20:12:03 +0900

    Make Sections

---
+++ main.js
@@ -0,0 +1,38 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth - 100;
+canvas.height = window.innerHeight - 100;
+
+
+var dino = {
+    x : 10,
+    y : 200,
+    width : 100,
+    height : 50,
+
+    draw (){
+        ctx.fillStyle = 'blue';
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+dino.draw()
+
+
+
+class Cactus {
+    constructor(){
+        this.x = 500;
+        this.y = 200;
+        this.width = 50;
+        this.height = 50;
+    }
+    draw(){
+        ctx.fillStyle = 'green';
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+var cactus = new Cactus();
+cactus.draw();
