ba924abd586349fd457dadfe6c134b44ae264374
Firoms <edisonmessi@naver.com>
2023-02-21 23:18:56 +0900

    Database Setting

--- FindGluttonousBear/game/models.py
+++ FindGluttonousBear/game/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Ranking(models.Model):
+    nickname = models.CharField(max_length=15, null=False)
+    score = models.IntegerField(default=0)
